1b07152488c95349e6938daf4937cf932a553185
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2026-03-25 15:17:38 +0200

    feat: create initial project scaffolding

---
+++ Linqr.CLI/App.cs
@@ -0,0 +1,85 @@
+﻿using JetBrains.Annotations;
+using Linqr.CLI.Core.Injection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Spectre.Console.Cli;
+
+namespace Linqr.CLI;
+
+public static class App
+{
+#region Lifecycle
+
+    /// <summary>
+    /// The main entry-point for the application.
+    /// </summary>
+    /// <param name="arguments">
+    /// Arguments parsed from the command-line upon execution of the application
+    /// via a terminal. Used by the <see cref="Spectre.Console.Cli.CommandApp"/>
+    /// to execute commands with arguments.
+    /// </param>
+    /// <returns>
+    /// <see cref="Environment.ExitCode"/>
+    /// </returns>
+    public static void Main(
+        string[] arguments
+    ) {
+        var configuration = BuildConfigurations();
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .Enrich.FromLogContext()
+            .CreateLogger();
+
+        try {
+            Log.Information("Application started");
+
+            var builder = Host.CreateDefaultBuilder(arguments);
+            builder
+                .UseSerilog()
+                .ConfigureServices((_, services) => {
+                    services.AddLogging(); //
+                });
+
+            var app = builder.BuildApplication();
+
+            app.Configure(options => {
+                options.SetApplicationName("linqr");
+                options.UseAssemblyInformationalVersion();
+            });
+
+            app.Run(arguments);
+
+            #if DEBUG
+            Console.ReadLine();
+            #endif
+        }
+        catch (Exception ex) {
+            Log.Fatal(ex, "Application terminated unexpectedly");
+        }
+        finally {
+            Log.Information("Application closing");
+            Log.CloseAndFlush();
+        }
+    }
+
+#endregion
+
+#region Internals
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    private static IConfiguration BuildConfigurations() {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("logging.json", optional: false, reloadOnChange: true)
+            .AddJsonFile("options.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        return builder.Build();
+    }
+
+#endregion
+}
---
+++ Linqr.CLI/Core/Injection/HostBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using Spectre.Console.Cli;
+
+namespace Linqr.CLI.Core.Injection;
+
+/// <summary>
+/// Extension methods for <see cref="IHostBuilder"/>.
+/// </summary>
+public static class HostBuilderExtensions
+{
+#region Extensions
+
+    /// <summary>
+    /// Creates a new <see cref="CommandApp"/> instance using the extended
+    /// <see cref="IHostBuilder"/> containing host configurations like services
+    /// and environments. This allows Dependency Injection (DI) to be used with
+    /// Spectre Console objects.
+    /// </summary>
+    /// <param name="builder">
+    /// The <see cref="IHostBuilder"/> to extend.
+    /// </param>
+    /// <returns>
+    /// <see cref="CommandApp"/>
+    /// </returns>
+    public static CommandApp BuildApplication(this IHostBuilder builder) {
+        var registrar = new SpectreTypeRegistrar(builder);
+        var application = new CommandApp(registrar);
+
+        return application;
+    }
+
+#endregion
+}
